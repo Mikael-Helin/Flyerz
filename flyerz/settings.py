21666e574ec2b2a32c4e8bc94f9c4ad887a8230d
andrtell <116459057+andrtell@users.noreply.github.com.>
2024-11-26 11:41:08 +0100

    moved base.html to public_pages app

--- flyerz/settings.py
+++ flyerz/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'flyerz.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates'],
+        'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
