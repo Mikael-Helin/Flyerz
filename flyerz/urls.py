7b651e5e063cfbedc7331cba7d5429213aa3e71a
andrtell <116459057+andrtell@users.noreply.github.com.>
2024-11-26 14:40:48 +0100

    add placeholder urls and views

--- flyerz/urls.py
+++ flyerz/urls.py
@@ -19,6 +19,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('users/', include('users.urls')),
     path('', include('public_pages.urls')),
     path('', include("django.contrib.auth.urls")),
 ]
