4f99ebd236266e0cd139b80539efe31a439b8c3c
Mikael <mikael@tuf17.mikaelhelin.com>
2024-11-28 08:09:19 +0100

    update

--- users/templatetags/custom_filters.py
+++ users/templatetags/custom_filters.py
@@ -3,11 +3,6 @@ from django import template
 register = template.Library()
 
 @register.filter
-def friendship_key(friendship_map, args):
-    """
-    Custom filter to fetch a value from friendship_map
-    using a concatenated key (e.g., 'user1-user2').
-    """
-    user1, user2 = args.split('-')
-    key = f"{user1}-{user2}"
+def friendship_key(friendship_map, key):
     return friendship_map.get(key, None)
+
