a9cc4550800427161b00f544393a0ce104300f10
andrtell <116459057+andrtell@users.noreply.github.com.>
2024-11-28 14:16:49 +0100

    filled out login form

--- users/forms.py
+++ users/forms.py
@@ -56,7 +56,9 @@ class UserRegisterForm(UserCreationForm):
 
 
 class LoginForm(forms.Form):
-    pass
+    username = forms.CharField(label="username", max_length=100)
+    password = forms.CharField(label="password", max_length=100)
+
 
 
 
