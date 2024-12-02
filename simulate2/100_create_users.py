c6c5a72e48291569942c9c80f4fa016456453b28
andrtell <116459057+andrtell@users.noreply.github.com.>
2024-12-02 14:55:06 +0100

    fake email

--- simulate2/100_create_users.py
+++ simulate2/100_create_users.py
@@ -6,18 +6,12 @@ from users.models import User
 fake = Faker()
 
 N = 10
-
-def create_email(first_name, last_name):
-    domain_name = fake.domain_name()
-    email = f"{first_name.lower()}.{last_name.lower()}@{domain_name}"
-    return email
-
 DEFAULT_PASSWORD="secret"
 
 for i in range(N):
     first_name = fake.first_name()
     last_name = fake.last_name()
-    email = create_email(first_name, last_name)    
+    email = fake.ascii_email()    
     username = f"{first_name.lower()}.{last_name.lower()}"
     user = User(
         username=username,
