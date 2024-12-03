be4b5062641299a2a647ae32275c9d4ade40b4fc
Silvia <trollunge86@gmail.com>
2024-12-03 08:37:23 +0100

    Updated url and profile for add event

--- events/urls.py
+++ events/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 
+app_name = 'events'
 urlpatterns =[
     path('', views.event, name='event'),
     path('add_event/', views.add_event, name='add_event'),
