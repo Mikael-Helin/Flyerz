9744b0ab67256a20a80681f4a206f85fb681699c
nikoletta <grishaberN@outlook.com>
2024-12-03 09:52:15 +0100

    Design events pages

--- events/urls.py
+++ events/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
 from . import views
 
+app_name = 'events'
 
 urlpatterns =[
-    path('', views.event, name='event'),
     path('add_event/', views.add_event, name='add_event'),
+    path('user_events/', views.user_events, name='user_events'),
+    path('edit/<int:event_id>/', views.edit_event, name='edit_event'),
+    path('delete/<int:event_id>/', views.delete_event, name='delete_event'),
+    path('event_list/', views.event_list, name='event_list'),
 ]
--- events/views.py
+++ events/views.py
@@ -1,24 +1,59 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 from .forms import EventForm
+from .models import Event
 
 # Create your views here.
-def event(request):
-    return render(request, 'events/event.html')
-
 def add_event(request):
     if request.method == 'POST':
         event_form = EventForm(request.POST)
-
         event_form.instance.organizer = request.user
 
         print('USER', request.user)
-         
+
         if event_form.is_valid():
-           
-           event_form.save()
-           return HttpResponse("Event added successfully.")
+            new_event = event_form.save()
+
+            events = Event.objects.filter(organizer=request.user)
+
+            return render(request, 'events/user_events.html', {'events': events})
     else:
         event_form = EventForm()
         print("GET request received")
+    events = Event.objects.filter(organizer=request.user)
     return render(request, 'events/add_event.html', {'event_form': event_form})
+
+
+def user_events(request):
+    events = Event.objects.filter(organizer=request.user)
+    return render(request, 'events/user_events.html', {'events': events})
+
+
+def event_list(request):
+    events = Event.objects.all()
+    return render(request, 'events/eventlist.html', {'events': events})
+
+
+@login_required
+def edit_event(request, event_id):
+    event = get_object_or_404(Event, id=event_id, organizer=request.user)
+    if request.method == 'POST':
+        form = EventForm(request.POST, instance=event)
+        if form.is_valid():
+            form.save()
+            return redirect('events:user_events')
+    else:
+        form = EventForm(instance=event)
+    return render(request, 'events/edit_event.html', {'form': form})
+
+
+@login_required
+def delete_event(request, event_id):
+    event = get_object_or_404(Event, id=event_id, organizer=request.user)
+    if request.method == 'POST':
+        event.delete()
+        return redirect('events:user_events')
+    
+    return render(request, 'events/confirm_delete.html', {'event': event})
