0d9fdaec015ac77157853a75dbb92d9f5163dd41
Silvia <trollunge86@gmail.com>
2024-12-05 09:34:12 +0100

    Add view in admin for Picture and Friendship

--- users/admin.py
+++ users/admin.py
@@ -3,7 +3,17 @@ from django.contrib.auth.admin import UserAdmin
 
 from .models import User
 
+class UserAdmin(UserAdmin):
+    model = User
+    
+    fieldsets = UserAdmin.fieldsets + (
+        ('Profile Picture', {'fields': ('profile_picture',)}),
+        ('Friendship Details', {'fields': ('friends',)}),
+    )
+    
+
 admin.site.register(User, UserAdmin)
+
 
 # ADDED BY MIKAEL
 # from .models import Friend
